ef8463416cc20bb52e0a1cf2cc055cc6c6b5eb49
Eli <eli@currah.com>
2026-04-09 20:43:17 -0700

    optimized page loading logic

--- instagram/instagram.py
+++ instagram/instagram.py
@@ -1,53 +1,60 @@
 import time
 import pyautogui
-import os
-
-# pyautogui.displayMousePosition()
 
 def wait():
     pyautogui.sleep(.3)
 
 def wait_for_reel():
-    pyautogui.sleep(10) # placeholder for real code
+    while True:
+        try:
+            heart_button = pyautogui.locateCenterOnScreen('heart.png', confidence=.9)
+            return heart_button
+        except pyautogui.ImageNotFoundException:
+            time.sleep(.1)
+            pass
+
 
 def wait_for_reload():
-    pyautogui.sleep(10) # placeholder for real code
+    while True:
+        try:
+            print(pyautogui.locateOnScreen('newest-to-oldest.png', confidence=.9))
+            time.sleep(.1)
+            return
+        except pyautogui.ImageNotFoundException:
+            time.sleep(.1)
+            pass
 
 # to-do list
 # make the wait functions optimal (image recognition)
 # ensure all video formats, displays (light mode), OS's? are supported
 # assess which prerequisite packages are required
 # write a goated README.md
-# add option to not have reels unliked
+# add option to not have reels unlikedq
 # add an option to keep friends reels liked
 # explore ways to make the bot overall faster
 #  - more tabs running
 
+# pyautogui.displayMousePosition()
+
 for i in range(2000):
 
     wait_for_reload()
-
-    # (900, 350) - new reel
     pyautogui.leftClick(900, 350)
-    wait_for_reel()
 
-    heart_button = pyautogui.locateCenterOnScreen('heart.png', confidence=.9)
-    print('heart button: ', heart_button)
+    heart_button = wait_for_reel()
     save_button = pyautogui.Point(heart_button.x + 280, heart_button.y)
-    print('save button: ', save_button)
     liked_playlist = pyautogui.Point(save_button.x, save_button.y - 240)
-    print('liked playlist: ', liked_playlist)
 
     pyautogui.moveTo(save_button, duration=.1)
-    pyautogui.sleep(1)
+    time.sleep(1)
+    wait()
 
     pyautogui.moveTo(liked_playlist, duration=.1)
     pyautogui.leftClick()
     wait()
 
     pyautogui.leftClick(heart_button)
-    pyautogui.sleep(1.5)
+    wait()
 
     pyautogui.hotkey('alt', 'left')
-
     wait()
